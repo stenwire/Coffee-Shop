03a3bfef9525c3e6db03c5e75a627a21d63d22a3
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-07-10 12:09:13 +0100

    [fix]: added new auth0

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-z4iv3gkc.us', // the auth0 domain prefix
+    url: 'dev-ix788c42.us', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
-    clientId: 'CAQ3qtJIA8Sd50skNIrhIdqMXepEl3dI', // the client id generated for the auth0 app
+    clientId: 'EhuHYVBNrs2OAI8P1s9vw93gzWi94Sqf', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
     // callbackURL: '/tabs/user-page',
   }
